d7789bde8bf8056ce792706013a20cd48482882d
sekharrao <m.sekhar.rao@gmail.com>
2017-06-12 17:46:14 +0100

    /helloInternal  service endpoint added for http service-to-service communication.

--- greetings/src/main/java/com/tandem/greetings/controller/GreetingsController.java
+++ greetings/src/main/java/com/tandem/greetings/controller/GreetingsController.java
@@ -2,6 +2,7 @@ package com.tandem.greetings.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GreetingsController {
@@ -9,6 +10,11 @@ public class GreetingsController {
 	@RequestMapping("/greetings")
 	public String sayHello(){
 		return "Hello World";
+	}
+	
+	@RequestMapping("/helloInternal")
+	public String callInteralService(){
+		return new RestTemplate().getForObject("http://localhost:8080/internal-greetings", String.class);
 	}
 
 }
